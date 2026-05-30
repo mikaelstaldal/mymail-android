d8b671da5d8c7db8f19279ac8bed4f479bb490c6
Mikael Ståldal <mikael@staldal.nu>
2026-05-30 18:19:41 +0200

    Fix compile target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "nu.staldal.mymail"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "nu.staldal.mymail"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -33,12 +33,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-
+    
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
 
     buildFeatures {
